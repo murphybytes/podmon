8d8a862b8c867037fee66b64a89dc48173658f7e
John Murphy <murphybytes@gmail.com>
2024-11-30 13:16:24 -0600

    wip

--- service/service_test.go
+++ service/service_test.go
@@ -2,44 +2,31 @@ package service
 
 import (
 	"context"
+	"sync"
 	"testing"
 
+	"github.com/murphybytes/pod-finder/service"
 	"github.com/stretchr/testify/assert"
-	"k8s.io/apimachinery/pkg/watch"
-	core "k8s.io/client-go/kubernetes/typed/core/v1"
 )
 
-type mockWatch struct {
-	ch chan watch.Event
-}
-
-func (m *mockWatch) ResultChan() <-chan watch.Event {
-	return m.ch
-}
-
-func (m *mockWatch) Stop() {}
-
-func mockPodGetter(_ context.Context, _ string, _ KeyValues, _ core.CoreV1Interface) (watch.Interface, error) {
-	return nil, nil
-}
-
-func getMockPodGetter(itf watch.Interface) PodGetter {
-	return func(_ context.Context, _ string, _ KeyValues, _ core.CoreV1Interface) (watch.Interface, error) {
-		return itf, nil
-	}
-}
-
 func TestService(t *testing.T) {
-	watcher := &mockWatch{
-		ch: make(chan watch.Event, 1),
-	}
-	var cfg Config
+	podMap := map[string]string{}
+	var lock sync.Mutex
+
 	ctx, fn := context.WithCancel(context.Background())
+
+	cfg := &service.Config{
+		OnModifiedPod: func(_ context.Context, podName, podIp string) {
+			lock.Lock()
+			defer lock.Unlock()
+			podMap[podName] = podIp
+		},
+	}
+
 	defer fn()
 	assert.Nil(t, Start(
 		ctx,
-		cfg,
-		WithPodGetterFunction(getMockPodGetter(watcher)),
+		&cfg,
 	))
 
 }
